7dd935ab7c13d1a350e540d186290c0673972557
Hackman9912 <charleshackett89@gmail.com>
2020-01-28 10:51:53 -0600

    Updated files to work better
    python script to properly read data, c code to get system, ip, user info.

--- systemInfo/getInfo.py
+++ systemInfo/getInfo.py
@@ -1,11 +1,13 @@
-import subprocess
-import pprint
+import subprocess ## import subprocess
 
-
-def main():
-    result = subprocess.check_output(['sysinfo.exe'])
-    result2 = subprocess.check_output(["ipInfoWindows.exe"])
-    pprint.pprint(result.decode("utf-8"))
-    pprint.pprint(result2.decode("utf-8"))
+def main(): ## define mine
+    ## set the variables to be the right subprocess thing
+    sysinfo = subprocess.check_output(['sysinfo.exe']).decode('ascii')
+    ipInfo = subprocess.check_output(["ipInfoWindows.exe"]).decode('ascii')
+    userInfo = subprocess.check_output(["sysUser.exe"]).decode('ascii')
+    ## print the data
+    print(sysinfo)
+    print(ipInfo)
+    print(userInfo)
 
 main()

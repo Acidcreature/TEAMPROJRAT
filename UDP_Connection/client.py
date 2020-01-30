3aafe3165ed9f52428061e046707d6ebd9ea59f7
rp729 <rp729@yahoo.com>
2020-01-30 13:53:28 -0500

    Organized directories and file names

--- UDP_Connection/client.py
+++ UDP_Connection/client.py
@@ -1,7 +1,24 @@
 from netconnect import *
 from gui2 import *
+import threading
+from pathlib import Path
+
+class Client:
+    def __init__(self):
+        pass
+
+    def client_gui(self):
+        exec(Path(f'client/client.py').read_text())
+
+    def client_listener(self):
+        exec(Path(f'client/client_listener.py').read_text())
+
+    def run(self):
+        t1 = threading.Thread(target=self.client_gui)
+        t2 = threading.Thread(target=self.client_listener)
+        t1.start()
+        t2.start()
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    my_gui = GUI(root)
-    root.mainloop()
+    client = Client()
+    client.run()
--- UDP_Connection/server.py
+++
@@ -1,6 +0,0 @@
-from netconnect import *
-if __name__ == '__main__':
-    host = 'localhost'
-    port = 5555
-    connect = NetConnect(host, port)
-    connect.server()

